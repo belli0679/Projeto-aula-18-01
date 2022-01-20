8b0ab94b5a1468db74f1d8cbe1493b7f6ae25113
belli0679 <93786545+belli0679@users.noreply.github.com>
2022-01-20 12:47:23 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,8 +5,10 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var canvas;
-var palyer, playerBase, playerArcher;
+var player, playerBase, playerArcher;
+var arrow;
 var baseimage;
+var playerimage;
 
 function preload() {
   backgroundImg = loadImage("./assets/background.png");
@@ -19,6 +21,7 @@ function setup() {
 
   engine = Engine.create();
   world = engine.world;
+
   angleMode(DEGREES);
 
   var options = {
@@ -29,20 +32,40 @@ function setup() {
   World.add(world, playerBase);
 
   player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
-  World.add(world, player);
+  World.add(world,player)
 
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.position.y - 112,
+    120,
+    120
+  );
+
+  arrow = new PlayerArrow(
+    playerArcher.body.position.x,
+    playerArcher.body.position.y,
+    100,
+    10
+  );
 }
 
 function draw() {
   background(backgroundImg);
-
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
   Engine.update(engine);
 
-  image(baseimage, playerBase.position.x, playerBase.position.y, 180, 150);
+  playerArcher.display();
+  arrow.display();
 
-  image(playerimage, player.position.x, player.position.y, 50, 180);
+  if (keyCode === 32) {
+    // arrow.(playerArcher.body.angle);
+    // arrow.shoot(playerArcher.angle);
+    arrow.shoot(playerArcher.body.angle);
+    // arrow.shoot(playerArcher);
+  }
 
-  // Title
+  // Título
   fill("#FFFF");
   textAlign("center");
   textSize(40);
